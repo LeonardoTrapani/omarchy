821830f54f6604d4f734ee942d285ba7f48210b3
LeonardoTrapani <leonard.trapani@gmail.com>
2025-08-02 00:23:48 +0200

    Restore after stow to keep symlinks up to date

--- install/os-dotfiles/stow.sh
+++ install/os-dotfiles/stow.sh
@@ -3,12 +3,13 @@ readonly CONFIGS=("aws" "bash" "git" "hypr" "nvim" "tmux")
 cd ~/os
 
 for config in "${CONFIGS[@]}"; do
-    if stow "$config" &> /dev/null; then
+    if stow --adopt "$config" &> /dev/null; then
         echo "Installed $config configuration"
     else
         echo "Failed to install $config configuration"
         exit 1
     fi
+    git restore . # Stow adopt creates the symlinks but gets the previously existing files, now we restore them to the actual ones
 done
 
 cd -
