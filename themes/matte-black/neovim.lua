18f3bafb3dd2938f626c7d103f5930f3da006e12
LeonardoTrapani <leonard.trapani@gmail.com>
2025-08-25 18:29:39 +0200

    minor changes

--- themes/matte-black/neovim.lua
+++ themes/matte-black/neovim.lua
@@ -1,9 +1,17 @@
+function ColorMyPencils(color)
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 return {
-	{ "tahayvr/matteblack.nvim", lazy = false, priority = 1000 },
 	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "matteblack",
-		},
+		"tahayvr/matteblack.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			ColorMyPencils("matteblack")
+		end,
 	},
 }
