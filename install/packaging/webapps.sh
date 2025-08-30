391607da17a75b3cfecf442e571df6cd5e74faee
LeonardoTrapani <leonard.trapani@gmail.com>
2025-08-30 19:03:04 +0200

    add google tasks webapp and shortcut

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -9,3 +9,4 @@ omarchy-webapp-install "X" https://x.com/ https://cdn.jsdelivr.net/gh/homarr-lab
 omarchy-webapp-install "Figma" https://figma.com/ https://www.veryicon.com/download/png/application/app-icon-7/figma-1?s=256
 omarchy-webapp-install "Discord" https://discord.com/channels/@me https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/discord.png
 omarchy-webapp-install "Zoom" https://app.zoom.us/wc/home https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/zoom.png
+omarchy-webapp-install "Tasks" https://tasks.google.com/tasks/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/todoist.svg
